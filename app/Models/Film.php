c30654c413a7049665b2bf3a135037194db38b7a
matthisRipoche <matthisripoche@gmail.com>
2026-04-15 15:27:34 +0200

    fix : model

--- app/Models/Film.php
+++ app/Models/Film.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 
 #[Fillable(['title', 'release_year', 'synopsis'])]
 class Film extends Model
--- app/Models/Location.php
+++ app/Models/Location.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 
 #[Fillable(['name', 'city', 'country', 'description', 'film_id', 'user_id', 'upvotes_count'])]
 class Location extends Model
